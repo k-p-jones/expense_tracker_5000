96a9187b0ed949d9b1d127b24aa8b2a2f511a8a7
k-p-jones <kenjones620@yahoo.co.uk>
2019-09-19 23:14:55 +0100

    Add authentication ------------------ * Add devise and generate User model. * Restrict routes so only sign in is allowed * Add the Dashboard page and set it as the site root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'dashboard#index'
+  # Block sign ups, Users can only be created from the console.
+  devise_for :users, skip: :registrations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
