39f9d16c60f705a1484049adc66d1f663bf9c88a
k-p-jones <kenjones620@yahoo.co.uk>
2019-10-06 16:53:09 -0700

    ensure axios sends the CSRF headers with every request

--- app/javascript/packs/expense_tracker.jsx
+++ app/javascript/packs/expense_tracker.jsx
@@ -5,8 +5,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ExpenseTracker from './src/containers/ExpenseTracker/ExpenseTracker';
+import setupCSRF from './src/config/setupCSRF';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Ensure that axios sends csrf token with every request
+  setupCSRF();
+
   ReactDOM.render(
     <ExpenseTracker />,
     document.body.appendChild(document.createElement('div')),
