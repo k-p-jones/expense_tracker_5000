5e748fd09e0d56a0d1482fdff828afe0086d6c89
k-p-jones <kenjones620@yahoo.co.uk>
2019-10-14 23:59:07 +0100

    add required packages for JS decorator syntax

--- babel.config.js
+++ babel.config.js
@@ -48,6 +48,7 @@ module.exports = function(api) {
       require('@babel/plugin-syntax-dynamic-import').default,
       isTestEnv && require('babel-plugin-dynamic-import-node'),
       require('@babel/plugin-transform-destructuring').default,
+      [require('@babel/plugin-proposal-decorators'), { "legacy": true }],
       [
         require('@babel/plugin-proposal-class-properties').default,
         {
