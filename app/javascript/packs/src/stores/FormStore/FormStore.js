4ceaf753f586858245c5adb463de6bf04ed00691
k-p-jones <kenjones620@yahoo.co.uk>
2019-10-19 22:55:41 +0100

    Creat a store for handling the form state.

---
+++ app/javascript/packs/src/stores/FormStore/FormStore.js
@@ -0,0 +1,47 @@
+import { action, observable } from 'mobx';
+
+class FormStore {
+  @observable showForm = false;
+  @observable description = '';
+  @observable date = '';
+  @observable editMode = false;
+  @observable selectedTransactionId = undefined;
+
+  @action toggleShow = () => {
+    this.showForm = !this.showForm;
+  }
+
+  @action toggleEdit = () => {
+    this.editMode = !this.editMode;
+  }
+
+  @action clear = () => {
+    this.description = '';
+    this.date = '';
+    this.cost = '';
+    this.selectedTransactionId = undefined;
+  }
+
+  @action populateEditForm = (description, cose, date, id) => {
+    this.description = description;
+    this.cost = cost;
+    this.date = date;
+    this.selectedTransactionId = id;
+  }
+
+  @action setDescription = (description) => {
+    this.description = description;
+  }
+
+  @action setDate = (date) => {
+    this.date = date;
+  }
+
+  @action setCost = (cost) => {
+    this.cost = cost;
+  }
+}
+
+const formStore = new FormStore();
+
+export default formStore;
